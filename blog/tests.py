d92557fa84be4209461c7d9ab7fc522b97b271ac
p-corteszaragoza <pcorteszaragoza@gmail.com>
2021-08-20 00:32:40 -0700

    Message Board App: Edit, Delete, Update

--- blog/tests.py
+++ blog/tests.py
@@ -37,4 +37,33 @@ class BlogTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(not_found_response.status_code, 404)
         self.assertContains(response, "A title")
-        self.assertTemplateUsed(response, "post_detail.html")        
+        self.assertTemplateUsed(response, "post_detail.html")     
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse("post_new"),{
+            "title": "New title",
+            "body": "New body",
+            "author": self.user.id
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New body")
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse("post_edit", args="1"),{
+            "title": "Updated title",
+            "body": "Updated body"
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
+    
+
+
+    
